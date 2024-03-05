cc14282a79247acc6c39ed1d68d5e983e0313cbe
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-03-05 19:45:37 +0000

    Delete unused components and images, update dependencies, and add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/pages/shared/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      gfhfg
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Home/>} />
+          </Routes>
+      </Router>
     </div>
   );
 }
