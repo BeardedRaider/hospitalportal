cae91b3708e42b786624ec603ae94545d28d2e11
Shane Cummings <Shanecummings_uk@yahoo.co.uk>
2024-03-19 13:14:22 +0000

    Refactor routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/pages/shared/Home';
-import Login from './components/pages/shared/Login';
-import Parent from './components/pages/parent/Parent';
-import Patient from './components/pages/patient/Patient';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/pages/shared/Home";
+import Login from "./components/pages/shared/Login";
+import Parent from "./components/pages/parent/Parent";
+import Patient from "./components/pages/patient/Patient";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-          <Routes>
-            <Route path='/' exact element={<Home/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/parent' element={<Parent/>} />
-            <Route path='/patient' element={<Patient/>} />
-          </Routes>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/parent" element={<Parent />} />
+          <Route path="/patient" element={<Patient />} />
+        </Routes>
       </Router>
     </div>
   );
