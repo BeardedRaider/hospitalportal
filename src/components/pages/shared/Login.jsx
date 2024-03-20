ee570513e1ec2eaee82dd631937238eb236533bf
Shane Cummings <Shanecummings_uk@yahoo.co.uk>
2024-03-20 11:46:11 +0000

    Refactor login form inputs

--- src/components/pages/shared/Login.jsx
+++ src/components/pages/shared/Login.jsx
@@ -69,28 +69,32 @@ useEffect(() => {
         <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
           <h1 class="text-2xl font-semibold mb-4">Login</h1>
           <form action="#" method="POST">
-            {/* <!-- Username Input --> */}
+            {/* <!-- -------Username Input------ --> */}
             <div class="mb-4">
-              <label for="username" class="block text-gray-600">
+              <label for="patient_number" class="block text-gray-600">
                 Username
               </label>
               <input
                 type="text"
-                id="username"
-                name="username"
+                id="patient_number"
+                value={patient_number}
+                onChange={(e) => setPatientNumber(e.target.value)}
+                placeholder="Patient Number"
                 class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
                 autocomplete="off"
               />
             </div>
-            {/* <!-- Password Input --> */}
+            {/* <!-------- Password Input --------> */}
             <div class="mb-4">
               <label for="password" class="block text-gray-600">
                 Password
               </label>
               <input
-                type="password"
                 id="password"
-                name="password"
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
                 autocomplete="off"
               />
