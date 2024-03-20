6163f51f00f6607be016e2f67b29b486aab77a70
Shane Cummings <Shanecummings_uk@yahoo.co.uk>
2024-03-20 09:28:13 +0000

    Refactor GameCard component and update game images

--- src/components/pages/patient/Games.js
+++ src/components/pages/patient/Games.js
@@ -4,10 +4,19 @@ import gameData from "../../../json/games.json";
 
 const Games = () => {
   return (
+    <div>
+    {gameData.map((game, index) => (
     <GameCard
-    
+    key={index}
+    image={game.image}
+    // url={game.url}
+    name={game.name}
+    description={game.description}
+      
     />
+    ))}
+    </div>
   );
-}
+};
 
 export default Games;
