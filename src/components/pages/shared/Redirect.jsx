2f819eb6685917f9c627935d1a00f1f2a9237eba
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-04-18 20:27:57 +0100

    Add redirect screen for users not using an iPad or Mobile

--- src/components/pages/shared/Redirect.jsx
+++ src/components/pages/shared/Redirect.jsx
@@ -5,7 +5,13 @@ function App () {
   return (
     <div className="App">
       <Navbar />
+
+      <div>
+        <h1>Redirecting...</h1>
+        <p>If you are not redirected, click <a href="/home">here</a>.</p>
       <img src="../../../../public/images/ipad-mobile.jpg" alt="" />
+      </div>
+
     </div>
   );
 }
