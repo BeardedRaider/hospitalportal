f2b07f16edf00d814bfe921c5916f2acd842480a
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-04-21 15:01:14 +0100

    Update video source and add link to talk with expert in Parent.jsx

--- src/components/pages/parent/Parent.jsx
+++ src/components/pages/parent/Parent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserInformation from '../../UserInformation'
 import "../../../styles/parent.css";
+import { Link } from 'react-router-dom';
 
 function Parent(){
     const user = UserInformation()
@@ -38,50 +39,52 @@ function Parent(){
 
 {/* --------TALK WITH US --------*/}
     <section className='bg-gray-200'>
-    <div className="py-12 relative overflow-hidden ">
-    <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
-          <div className="w-full flex flex-col items-end pr-16">
-            <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance on how medical procedures work</h2>
-            <div className="h-full mt-auto overflow-hidden relative">
-              <video controls autoPlay loop className="h-full w-full object-cover docVid">
-                <source src="https://www.pexels.com/download/video/5426204/" type="video/webm" />
-                
-                Your browser does not support the video tag.
-              </video>
+      {/* Into + vid */}
+      <div className="py-12 relative overflow-hidden ">
+        <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
+              <div className="w-full flex flex-col items-end pr-16">
+                <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance on how medical procedures work</h2>
+                <div className="h-full mt-auto overflow-hidden relative">
+                  <video controls autoPlay loop className="h-full w-full object-cover docVid">
+                    <source src="https://www.pexels.com/download/video/5426204/" type="video/webm" />
+                    
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+              </div>
+
+          <div className="py-20 rightBox relative ">
+            <div className="relative z-20 pl-12">
+              <h2 className="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Rainbow Medical is here <br/>to help you.</h2>
+              <p className="text-white text-sm mr-10">
+                Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+              </p>
+              <button className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white">Talk with expert</button>
+              
             </div>
           </div>
-
-      <div className="py-20 rightBox relative ">
-        <div className="relative z-20 pl-12">
-          <h2 className="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Rainbow Medical is here <br/>to help you.</h2>
-          <p className="text-white text-sm mr-10">
-            Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
-          </p>
-          <button className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
-          
         </div>
       </div>
-    </div>
-  </div>
 
-    {/* How to get to us */}
+    {/* Upcomming Appointments*/}
 
-  {/* background of second part of section */}
   <div class="py-4 relative overflow-hidden bg-gray-100">
     <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
     
 
       <div class="py-20 leftBox relative ">
         <div class="relative z-20 pl-12">
-          <h2 class="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here to help you</h2>
-          <p class="text-sky-950 text-sm mr-10">
-            Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+          <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Your Childs Appointments</h2>
+          <p class=" text-white text-sm mr-10">
+            Here, you can find everthing you need to know about your child's upcoming appointments. You can also find out how to prepare for the appointments and what to expect.
           </p>
-          <button class="mt-8 text-sky-950 uppercase py-3 text-sm px-10 border border-sky-950 hover:bg-white hover:bg-opacity-10">Talk with expert</button>
+          <button class="mt-8 text-white uppercase py-3 text-sm px-10 border" >
+          <Link to="/Appointments">Talk with expert</Link>
+            </button>
         </div>
       </div>
       <div class="w-full flex flex-col pl-16">
-        <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Whether you need Assistance</h2>
+        <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Getting Help where <br/> you need it.</h2>
         <div class="h-full mt-auto overflow-hidden relative">
           <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
         </div>
@@ -106,7 +109,7 @@ function Parent(){
           <p class="text-white text-sm mr-10">
               Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
           </p>
-          <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
+          <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white ">Talk with expert</button>
         </div>
       </div>
     </div>
