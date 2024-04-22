6233b4023dabc3cae452224b091da675dda0880f
Shane Cummings <Shanecummings_uk@yahoo.co.uk>
2024-04-22 13:43:01 +0100

    Refactor navigation layout and move login to the end

--- src/components/pages/parent/Parent.jsx
+++ src/components/pages/parent/Parent.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import UserInformation from '../../UserInformation'
-import "../../../styles/parent.css";
 import { Link } from 'react-router-dom';
+import "../../../styles/parent.css";
+
 
 function Parent(){
     const user = UserInformation()
+
+
 
   return (  
   <div>
@@ -36,18 +39,16 @@ function Parent(){
           </div>
       </div>
     </section>
-
-{/* --------TALK WITH US --------*/}
+    {/* --------TALK WITH US --------*/}
     <section className='bg-gray-200'>
       {/* Into + vid */}
-      <div className="py-12 relative overflow-hidden ">
+      <div className="py-12 relative overflow-hidden">
         <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
               <div className="w-full flex flex-col items-end pr-16">
                 <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance on how medical procedures work</h2>
                 <div className="h-full mt-auto overflow-hidden relative">
                   <video controls autoPlay loop className="h-full w-full object-cover docVid">
                     <source src="https://www.pexels.com/download/video/5426204/" type="video/webm" />
-                    
                     Your browser does not support the video tag.
                   </video>
                 </div>
@@ -66,57 +67,84 @@ function Parent(){
         </div>
       </div>
 
-    {/* Upcomming Appointments*/}
+      {/* Upcomming Appointments*/}
 
-  <div class="py-4 relative overflow-hidden bg-gray-100">
-    <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+      <div class="py-4 relative overflow-hidden bg-gray-100">
+        <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+        
+
+          <div class="py-20 leftBox relative ">
+            <div class="relative z-20 pl-12">
+              <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Your Childs Appointments</h2>
+              <p class=" text-white text-sm mr-10">
+                Here, you can find everthing you need to know about your child's upcoming appointments. You can also find out how to prepare for the appointments and what to expect.
+              </p>
+              <button class="mt-8 text-white uppercase py-3 text-sm px-10 border" >
+              <Link to="/Appointments">Go To Appointments</Link>
+                </button>
+            </div>
+          </div>
+          <div class="w-full flex flex-col pl-16">
+            <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Getting Help where <br/> you need it.</h2>
+            <div class="h-full mt-auto overflow-hidden relative">
+              <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+        {/* Our facilities */}
+      <div class="py-12 relative overflow-hidden ">
+        <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
+          <div class="w-full flex flex-col items-end pr-16">
+            <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">What's On The <br/> Grounds?
+            </h2>
+            <div class="h-full mt-auto overflow-hidden relative">
+              <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
+          </div>
+        </div>
+          <div class="py-20 rightBox relative">
+            <div class="relative z-20 pl-12">
+              <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Explore Our Facilities</h2>
+              <p class="text-white text-sm mr-10">
+                  Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+              </p>
+              <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white ">Take A Look Around</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      
+    </section>
+    {/* ------MAP------ */}
+    <section>
     
-
-      <div class="py-20 leftBox relative ">
-        <div class="relative z-20 pl-12">
-          <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Your Childs Appointments</h2>
-          <p class=" text-white text-sm mr-10">
-            Here, you can find everthing you need to know about your child's upcoming appointments. You can also find out how to prepare for the appointments and what to expect.
-          </p>
-          <button class="mt-8 text-white uppercase py-3 text-sm px-10 border" >
-          <Link to="/Appointments">Talk with expert</Link>
-            </button>
-        </div>
-      </div>
-      <div class="w-full flex flex-col pl-16">
-        <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Getting Help where <br/> you need it.</h2>
-        <div class="h-full mt-auto overflow-hidden relative">
-          <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-    {/* Our facilities */}
-  <div class="py-12 relative overflow-hidden ">
-    <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
-      <div class="w-full flex flex-col items-end pr-16">
-        <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance
-        </h2>
-        <div class="h-full mt-auto overflow-hidden relative">
-          <img src="https://picsum.photos/800/600" class="h-full w-full object-contain" alt=""/>
-      </div>
-    </div>
-      <div class="py-20 rightBox relative">
-        <div class="relative z-20 pl-12">
-          <h2 class="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Finpoint is here to help you</h2>
-          <p class="text-white text-sm mr-10">
-              Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
-          </p>
-          <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white ">Talk with expert</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-</section>
+    <div className="py-12 relative overflow-hidden rightBox">
+            <div className="max-w-screen-lg mx-auto">
+              <div className="w-full flex flex-col items-center">
+                <h2 className="text-[#f7d0b6] font-bold text-2xl mb-12 mt-10">Come And Find Us!</h2>
+                <div className="border rounded-lg overflow-hidden mb-8">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4482.16827465663!2d-4.2618023!3d55.8264989!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488846f0764625f9%3A0xd772f35465473b17!2sNew%20Victoria%20Hospital!5e0!3m2!1sen!2suk!4v1713788438143!5m2!1sen!2suk"
+                    width="600"
+                    height="450"
+                    style={{ border: '0' }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="Hospital Location">
+                  </iframe>
+                </div>
+                <a href="https://www.google.com/maps" target="_blank" rel="noreferrer">
+                  <button className="text-white uppercase py-2 text-sm px-4 border border-white bg-gray-900">Navigate</button>
+                </a>
+              </div>
+            </div>
+          </div>
+    </section>
 
 </div>
   );
