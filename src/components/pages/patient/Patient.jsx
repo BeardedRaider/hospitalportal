c5d55ff0083e9fa6f7cd24bd8d8923582631f8ea
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-04-24 10:31:30 +0100

    Refactor CSS classes in Patient.jsx and parent.css, and add new patient section in patient.css

--- src/components/pages/patient/Patient.jsx
+++ src/components/pages/patient/Patient.jsx
@@ -7,9 +7,9 @@ function Patient(){
 
   return (
     <div>
-        <section class="bg-gray-300 py-24 px-4 lg:px-16"> 
-            <div class="container mx-auto px-[12px] md:px-24 xl:px-12 max-w-[1300px] nanum2">
-                <h1 class="text-center text-5xl pb-12">Welcome
+        <section className=" patientSection bg-gray-300 py-24 px-4 lg:px-16"> 
+            <div className="container mx-auto px-[12px] md:px-24 xl:px-12 max-w-[1300px] nanum2">
+                <h1 className="text-center text-5xl pb-12">Welcome
                 </h1>
                 <h2 className="text-3xl md:text-4xl leading-relaxed md:leading-snug mb-2">
                     {user ? user.firstname : 'Loading...'}!
