070f7da53e5fd855d0a1c491a4a1b882bf709467
BeardedRaider <shanecummings_uk@yahoo.co.uk>
2024-04-24 11:50:39 +0100

    Refactor Patient.jsx, CSS classes, Appointments.jsx, Games.js, GameCard.jsx, and Parent.jsx

--- src/components/pages/patient/Patient.jsx
+++ src/components/pages/patient/Patient.jsx
@@ -21,7 +21,7 @@ function Patient(){
 
         {/* ---------ABOUT US---------- */}
 
-    <section className='subIntro'>
+    <section className='subIntro '>
       <div className=" py-20 ml-10">
         <div className="max-w-screen-lg mx-auto flex justify-between items-center">
           <div className="max-w-xl">
@@ -34,25 +34,25 @@ function Patient(){
 
     {/* --------TALK WITH US --------*/}
 
-    <section className='bg-gray-200'>
+    <section className='bg-gray-200 '>
       {/* Into + vid */}
     <div className="py-12 relative overflow-hidden">
         <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
             <div className="w-full flex flex-col items-end pr-16">
-                <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance on how medical procedures work</h2>
+                <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Come and see whats at the hospital!</h2>
                 <div className="h-full mt-auto overflow-hidden relative">
-                  <video controls autoPlay loop className="h-full w-full object-cover docVid">
-                    <source src="https://videos.pexels.com/video-files/8612560/8612560-hd_1920_1080_25fps.mp4" type="video/webm" />
-                    Your browser does not support the video tag.
+                  <video controls autoPlay loop className="h-full w-full object-cover docVid rounded-r-full ">
+                    <source src="https://videos.pexels.com/video-files/8612560/8612560-hd_1920_1080_25fps.mp4 " type="video/webm" />
+                    Your browser does not support the video.
                   </video>
                 </div>
             </div>
 
-            <div class="py-20 rightBox relative rounded-l-full">
+            <div class="py-20 rightBox relative rounded-l-full shadow-2xl">
             <div class="relative z-20 pl-16 ml-10">
-                    <h2 className="text-[#f7d0b6] font-black text-3xl leading-snug mb-10">Rainbow Medical is here <br/>to help you.</h2>
+                    <h2 className="text-[#f7d0b6] font-black text-3xl leading-snug mb-10">We are here <br/>to help you feel relaxed about your visit.</h2>
                     <p className="text-white text-sm mr-10">
-                        Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+                        Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra.
                     </p>
                     <button className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white">Talk with expert</button>
                 </div>
@@ -65,9 +65,9 @@ function Patient(){
     <div class="py-4 relative overflow-hidden bg-gray-100">
         <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
         
-          <div class="py-20 leftBox relative rounded-r-full">
+          <div class="py-20 leftBox relative rounded-r-full shadow-2xl">
             <div class="relative z-20 pl-12 ">
-              <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Your Childs Appointments</h2>
+              <h2 class="text-[#f7d0b6] font-black text-3xl leading-snug mb-10">Your Appointment</h2>
               <p class=" text-white text-sm mr-10">
                 Here, you can find everthing you need to know about your child's upcoming appointments. You can also find out how to prepare for the appointments and what to expect.
               </p>
@@ -77,67 +77,41 @@ function Patient(){
             </div>
           </div>
           <div class="w-full flex flex-col pl-16">
-            <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Getting Help where <br/> you need it.</h2>
+            <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Whan to know more <br/> about your appointment?</h2>
 
             <div class="h-full mt-auto overflow-hidden relative">
-            <img src="/ballPit.jpg" className="h-full w-full object-contain" alt="" />            
+            <img src="/images/appointment.jpg"className="h-full w-full object-contain rounded-l-full" alt="" />            
             </div>
           </div>
         </div>
       </div>
 
-  {/*-------- Our facilities -------- */}
-      <div class="py-12 relative overflow-hidden ">
+  {/*-------- Entertainment -------- */}
+      <div class="py-12 relative overflow-hidden " style={{ boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)' }}>
         <div class="grid grid-cols-2 max-w-screen-lg mx-auto">
           <div class="w-full flex flex-col items-end pr-16">
-            <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">What's On The <br/> Grounds?
+            <h2 class="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">What can you do <br/> while waiting at the hospital?
             </h2>
             <div class="h-full mt-auto overflow-hidden relative">
-            <img src="/images/bed.jpg" className="h-full w-full object-contain" alt="" />            
+            <img src="/images/bed.jpg" className="h-full w-full object-contain rounded-r-full" alt="" />            
             </div>
         </div>
-          <div class="py-20 rightBox relative rounded-l-full">
+          <div class="py-20 rightBox relative rounded-l-full shadow-2xl">
             <div class="relative z-20 pl-16 ml-10">
-              <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Explore Our Facilities</h2>
+              <h2 class="text-[#f7d0b6] font-black text-4xl leading-snug mb-10">Play Some Awesome Games!</h2>
               <p class="text-white text-sm mr-10">
-                  Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+                  Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie.
               </p>
-              <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white ">Take A Look Around</button>
+                <button class="mt-8 text-white uppercase py-3 text-sm px-10 border border-white ">
+                <Link to="/Games">Start Gaming</Link>
+                </button>
             </div>
           </div>
         </div>
       </div>
 
+    </section>
 
-      
-    </section>
-    
-    {/* ------MAP------ */}
-    <section>
-        <div className="py-12 relative overflow-hidden rightBox">
-        <div className="max-w-screen-lg mx-auto">
-            <div className="w-full flex flex-col items-center">
-                <h2 className="text-[#f7d0b6] font-bold text-2xl mb-12 mt-10">Come And Find Us!</h2>
-                <div className="relative z-20 pl-12border rounded-lg overflow-hidden mb-8">
-                    <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4482.16827465663!2d-4.2618023!3d55.8264989!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488846f0764625f9%3A0xd772f35465473b17!2sNew%20Victoria%20Hospital!5e0!3m2!1sen!2suk!4v1713788438143!5m2!1sen!2suk"
-                    width="750"
-                    height="400"
-                    style={{ border: '0' }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    title="Hospital Location">
-                        <button className="text-white uppercase py-2 text-sm px-4 border border-white bg-gray-900"></button>
-                    </iframe>
-                </div>
-                <p class="text-white text-lg mr-10 ml-10">
-                    Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie.
-                </p>
-            </div>
-        </div>
-        </div>
-    </section>
 
     </div>
     )
